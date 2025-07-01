7c555caeefcf84301aed2bb8a6e1e1f5448dc129
larry <larryking8118@gmail.com>
2025-07-01 18:48:09 +0000

    Setup: initial project scaffold and folder structure created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { CountryDetailsComponent } from './components/country-details-component/country-details-component';
+import { CountryListComponent } from './components/country-list-component/country-list-component';
+import { NotFound } from './components/not-found/not-found';
+import { LandingPage } from './components/landing-page/landing-page';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: CountryListComponent,
+        title: 'All countries'
+    },
+    {
+        path: 'country-details',
+        component: CountryDetailsComponent,
+        title: 'Country details'
+    },
+    {
+        path: 'Welcome',
+        component: LandingPage,
+        title: 'Landing page'
+    },
+    {
+        path: '**',
+        component: NotFound,
+        title: '404 Not Found'
+    }
+];
---
+++ src/app/components/theme-switcher-component/theme-switcher-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-switcher-component',
+  imports: [],
+  templateUrl: './theme-switcher-component.html',
+  styleUrl: './theme-switcher-component.scss'
+})
+export class ThemeSwitcherComponent {
+
+}
