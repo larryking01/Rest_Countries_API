c583e383f4d6ab30de419215b326d97e9c805ca3
larry <larryking8118@gmail.com>
2025-07-04 22:33:53 +0000

    Feature: Refactor display countries list

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { ThemeSwitcherComponent } from '../theme-switcher-component/theme-switcher-component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [ThemeSwitcherComponent],
+  imports: [ThemeSwitcherComponent, RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
